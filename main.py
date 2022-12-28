29ae36a1d52099e4094def45e3232b06116cba60
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2022-12-28 19:12:56 +0530

    final commited changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import *
 from test import get_res
 from states import get_address
-from pydantic import BaseModel
 from googletrans import Translator
+from fastapi.responses import PlainTextResponse
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 translator = Translator()
@@ -18,45 +20,64 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Message(BaseModel):
+    msg:str
+    lang:str
+
+class Address(BaseModel):
+    pincode : str
+    lang : str
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
 
 @app.get("/translater")
-async def translater(msg,lang):
-    result = translator.translate(msg,dest=lang)
+async def translater(message:Message):
+    print(message)
+    result = translator.translate(message.msg,dest=message.lang)
     return str(result.text)
 
 
 @app.get("/translation")
-async def translation(msg,lang):
+async def translation(message:Message):
     res = []
-    messages = msg.split(",")
+    print(message)
+    messages = message.msg.split(",")
     for a in messages:
-        res.append(translator.translate(a,dest=lang).text)
+        res.append(translator.translate(a,dest=message.lang).text)
     return res
 
 @app.get("/address")
-async def getAddress(pincode,lang):
+async def getAddress(address : Address):
     res = []
-    result1 = translator.translate("The Nearest Store is : ",dest=lang).text
-    messages = get_address(int(pincode))
+    result1 = translator.translate("The Nearest Store is : ",dest=address.lang).text
+    messages = get_address(int(address.pincode))
     for a in messages:
-        res.append(translator.translate(a,dest=lang).text)
+        res.append(translator.translate(a,dest=address.lang).text)
     return result1 + res[0]
 
 @app.get("/get_response")
-async def getResponse(msg,lang):
+async def getResponse(message:Message):
+    print(message)
     index = -5
-    if "help" in msg:
+    msg1 = message.msg
+    if "help" in msg1:
         index=2
-    elif "hi" in msg or "hello" in msg:
+    elif "hi" in msg1 or "hello" in msg1:
         index=1
-    elif "bye" in msg:
+    elif "bye" in msg1:
         index=3
     else:
         index=4
 
     if index == 4:
-        res = translator.translate("please enter the correct input",dest=lang).text
+        res = translator.translate("please enter the correct input",dest=message.lang).text
     else:
         r = get_res(index)
-        res = translator.translate(r,dest=lang).text
+        res = translator.translate(r,dest=message.lang).text
     return res
+
+@app.get("/")
+async def root():
+    return {"message": "this is chatbot-response-server"}
